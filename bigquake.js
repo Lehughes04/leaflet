7d5d75ff9c7a8a092105122061a641164d4c307c
Lee Hughes <lev.hughes09@gmail.com>
2018-03-14 23:26:09 -0400

    frustration

--- bigquake.js
+++ bigquake.js
@@ -1,35 +1,51 @@
-// Store our API endpoint inside queryUrl
-//var queryUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-//  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var plate = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
 });
 
+
+
+var plates = new L.layerGroup();
+ d3.json(plate, function (response) {
+    function styling(feature) {
+      return {
+        fillColor: 'steelblue',
+        weight: 1,
+        opacity: .7,
+        color: 'yellow', 
+        fillOpacity: 0.2
+      };
+    }
+
+    L.geoJSON(response, {
+      style: styling
+    }).addTo(plates);
+	plates.addTo(myMap);
+})
+
 function createFeatures(earthquakeData) {
 
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
+  
   function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.place +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
   }
 
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
+  
   var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature
   });
 
-  // Sending our earthquakes layer to the createMap function
+  
   createMap(earthquakes);
 }
 
 function createMap(earthquakes) {
 
-  // Define streetmap and darkmap layers
+  
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
     "access_token=pk.eyJ1IjoibGVodWdoZXMwNCIsImEiOiJjamVxaWxvenYwcG4wMnZwZ3plYjdmYjNwIn0.1DDfAxtSM5ipCStcNLNCvA");
 	
@@ -42,7 +58,7 @@ function createMap(earthquakes) {
   var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?" +
     "access_token=pk.eyJ1IjoibGVodWdoZXMwNCIsImEiOiJjamVxaWxvenYwcG4wMnZwZ3plYjdmYjNwIn0.1DDfAxtSM5ipCStcNLNCvA");
 	
-  // Define a baseMaps object to hold our base layers
+  
   var baseMaps = {
     "Street Map": streetmap,
     "Dark Map": darkmap,
@@ -50,23 +66,22 @@ function createMap(earthquakes) {
 	"Sat Map": satellitemap
   };
 
-  // Create overlay object to hold our overlay layer
+  
   var overlayMaps = {
-    Earthquakes: earthquakes
+    Earthquakes: earthquakes,
+	"Tectonic Plates": plates
   };
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  
   var myMap = L.map("map", {
     center: [
       37.09, -95.71
     ],
     zoom: 3,
-    layers: [lightmap, earthquakes]
+    layers: [lightmap, earthquakes, plates]
   });
 
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
+  
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
